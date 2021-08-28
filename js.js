4a5e2a96cf2c42ac05827e591789ef9290eb8914
Arzhan Bakytbek <79097350+roses-are-dead666@users.noreply.github.com>
2021-08-28 17:19:27 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,40 @@
-const display = document.getElementById("clock");
+const display = document.getElementById('clock');
+const audio = new Audio('./assets/alarm.mp3');
+audio.loop = true;
+let alarmTime = null;
+let alarmTimeout = null;
+
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+
+const today = new Date();
+
+document.getElementById("today").innerHTML =
+("Today is " + dateSuffix(today.getDate()) + " of " + monthNames[today.getMonth()] + ", " + today.getFullYear());
+
+function dateSuffix(i) {
+    var day1 = i % 10,
+        day2 = i % 100;
+    if (day1 == 1 && day2 != 11) {
+        return i + "st";
+    }
+    if (day1 == 2 && day2 != 12) {
+        return i + "nd";
+    }
+    if (day1 == 3 && day2 != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
 
 function updateTime() {
     const date = new Date();
-
-    const hours =   formatTime(date.getHours());
+    const hour = formatTime(date.getHours());
     const minutes = formatTime(date.getMinutes());
     const seconds = formatTime(date.getSeconds());
 
-    display.innerText = `${hours} : ${minutes} : ${seconds}`;
+    display.innerText=`${hour} : ${minutes} : ${seconds}`
 }
 
 function formatTime(time) {
@@ -15,6 +42,31 @@ function formatTime(time) {
         return '0' + time;
     }
     return time;
+}
+
+function setAlarmTime(value) {
+    alarmTime = value;
+}
+
+function setAlarm() {
+    if(alarmTime) {
+        const current = new Date();
+        const timeToAlarm = new Date(alarmTime);
+
+        if (timeToAlarm > current) {
+            const timeout = timeToAlarm.getTime() - current.getTime();
+            alarmTimeout = setTimeout(() => audio.play(), timeout);
+            alert('Alarm set');
+        }
+    }
+}
+
+function clearAlarm() {
+    audio.pause();
+    if (alarmTimeout) {
+        clearTimeout(alarmTimeout);
+        alert('Alarm cleared');
+    }
 }
 
 setInterval(updateTime, 1000);
